aacb5532a602c735858adf64aa345ed38655ead8
SammyObeezy <barasam07@gmail.com>
2025-01-13 14:18:08 +0300

    Process of updating UI, fetching backend data from prisma, updated Students, teachers folder files

--- Fullstack-school-mgmt/src/app/(dashboard)/list/students/page.tsx
+++ Fullstack-school-mgmt/src/app/(dashboard)/list/students/page.tsx
@@ -2,45 +2,39 @@ import FormModel from '@/components/FormModel';
 import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
-import { role, studentsData} from '@/lib/data';
+import { role, studentsData } from '@/lib/data';
+import prisma from '@/lib/prisma';
+import { ITEMS_PER_PAGE } from '@/lib/settings';
+import { Prisma } from '@prisma/client';
 import Image from 'next/image';
 import Link from 'next/link';
 
-type Student = {
-  id:number;
-  studentId:string;
-  name:string;
-  email?:string;
-  photo:string;
-  phone?:string;
-  grade:number;
-  class:string;
-  address:string;
-}
+type StudentList = Student & {class:Class};
+
 const columns = [
   {
-    header:"Info", 
-    accessor:"Info"
+    header: "Info",
+    accessor: "Info"
   },
   {
-    header:"Student ID", 
-    accessor:"studentId", 
-    className:"hidden md:table-cell"
+    header: "Student ID",
+    accessor: "studentId",
+    className: "hidden md:table-cell"
   },
   {
-    header:"Grades", 
-    accessor:"grade", 
-    className:"hidden md:table-cell"
+    header: "Grades",
+    accessor: "grade",
+    className: "hidden md:table-cell"
   },
   {
-    header:"Phone", 
-    accessor:"phone", 
-    className:"hidden lg:table-cell"
+    header: "Phone",
+    accessor: "phone",
+    className: "hidden lg:table-cell"
   },
   {
-    header:"Address", 
-    accessor:"address", 
-    className:"hidden lg:table-cell"
+    header: "Address",
+    accessor: "address",
+    className: "hidden lg:table-cell"
   },
   {
     header: "Actions",
@@ -49,38 +43,83 @@ const columns = [
 ]
 
 
-const StudentListPage = () => {
-
-  const renderRow = (item:Student) =>(
-   <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'>
-     <td className='flex items-center gap-4 p-4'>
-      <Image 
-      src={item.photo} 
-      alt='' 
-      width={40} 
-      height={40} 
-      className="md:hidden xl:block w-10 h-10 rounded-full object-cover" />
+const renderRow = (item: StudentList) => (
+  <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'>
+    <td className='flex items-center gap-4 p-4'>
+      <Image
+        src={item.img || "/noAvatar.png"}
+        alt=''
+        width={40}
+        height={40}
+        className="md:hidden xl:block w-10 h-10 rounded-full object-cover" />
       <div className="flex flex-col">
         <h3 className='font-semibold'>{item.name}</h3>
-        <p className='text-xs text-gray-500'>{item?.class}</p>
+        <p className='text-xs text-gray-500'>{item.class.name}</p>
       </div>
     </td>
-    <td className='hidden md:table-cell'>{item.studentId}</td>
-    <td className='hidden md:table-cell'>{item.grade}</td>
+    <td className='hidden md:table-cell'>{item.username}</td>
+    <td className='hidden md:table-cell'>{item.class.name[0]}</td>
     <td className='hidden md:table-cell'>{item.phone}</td>
     <td className='hidden md:table-cell'>{item.address}</td>
     <td>
       <div className="flex items-center gap-2">
-       {role === "admin" && (
-        <>
-      <FormModel table='parent' type='update' data={item} />
-      <FormModel table='student' type='delete' id={item.id} />
-      </>
-       )}
+        <Link href={`/list/students/${item.id}`}>
+
+        </Link>
+        {role === "admin" && (
+          <>
+            {/* <FormModel table='parent' type='update' data={item} /> */}
+            <FormModel table='student' type='delete' id={item.id} />
+          </>
+        )}
       </div>
     </td>
-   </tr>
-  );
+  </tr>
+);
+
+const StudentListPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
+
+  const { page, ...queryParams } = searchParams;
+
+  const p = page ? parseInt(page) : 1;
+
+  // UTL PARAMS CONDITION
+
+  const query: Prisma.StudentWhereInput = {}
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "teacherId":
+            query.class = {
+              lessons: {
+                some: {
+                  teacherId: value,
+                },
+              },
+            };
+            break;
+          case "search":
+            query.name = { contains: value, mode: "insensitive" }
+        }
+      }
+    }
+  }
+  const [data, count] = await prisma.$transaction([
+    prisma.student.findMany({
+      where: query,
+      include: {
+        class: true,
+      },
+      take: ITEMS_PER_PAGE,
+      skip: ITEMS_PER_PAGE * (p - 1),
+    }),
+    prisma.student.count({ where: query }),
+  ]);
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/*TOP */}
@@ -96,15 +135,15 @@ const StudentListPage = () => {
               <Image src="/sort.png" alt='' width={14} height={14} />
             </button>
             {role === "admin" && (
-                <FormModel table='teacher' type='create' />
-           ) }
+              <FormModel table='teacher' type='create' />
+            )}
           </div>
         </div>
       </div>
       {/*LIST */}
-      <Table columns={columns} renderRow={renderRow} data={studentsData} />
+      <Table columns={columns} renderRow={renderRow} data={data} />
       {/*PAGINATION */}
-        <Pagination />
+      <Pagination page={p} count={count} />
     </div>
   )
 }
